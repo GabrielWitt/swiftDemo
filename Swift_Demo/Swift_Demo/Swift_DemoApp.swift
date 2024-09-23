8bed8d8ed75d3e816d06da32b80dd10f0f55a6e6
Gabriel Witt <gabrowitt@gmail.com>
2024-09-22 21:18:29 -0500

    Initial commit

---
+++ Swift_Demo/Swift_Demo/Swift_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_DemoApp.swift
+//  Swift_Demo
+//
+//  Created by Gabo Witt on 22/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
